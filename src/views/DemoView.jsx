c0adcc4f4e5b08cc525da0ea13d4c7a92a6f1138
adam russell <badamrussell@users.noreply.github.com>
2022-05-20 12:54:30 -0400

    removes temporary use of DemoView

--- src/views/DemoView.jsx
+++ src/views/DemoView.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
-import { Demo } from "@adam/component-library";
-
 
 export const DemoView = () => (
   <div>
     <h1>This is your brand new application scaffold!</h1>
-    <Demo />
   </div>
 );
