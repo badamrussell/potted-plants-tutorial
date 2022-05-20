75dacd13c21b83d1ea16733d4f17765ce54b7072
adam russell <badamrussell@users.noreply.github.com>
2022-05-20 18:52:54 -0400

    uses github component library

--- src/views/DemoView.jsx
+++ src/views/DemoView.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import { Demo } from "@adam/component-library";
+
 
 export const DemoView = () => (
   <div>
     <h1>This is my application!</h1>
+    <Demo />
   </div>
 );
